617310cb88a1e1e4e3b23d73455eff53892c3c48
Luis Henrique Witz <luiswitz@gmail.com>
2017-10-07 17:59:58 -0300

    Add controllers and RaffleMailer

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,19 @@
+class CampaignsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def raffle
+  end
+end
