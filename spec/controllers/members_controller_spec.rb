210c041cc184ef13ab86d9fc27784b8d0f77a915
Luis Henrique Witz <luis.witz@xing.com>
2017-11-30 22:52:44 -0200

    Add tests for create members method

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,22 +2,36 @@ require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:user) { FactoryGirl.create(:user) }
+
+  before { sign_in user }
+
+  describe "POST #create" do
+    context 'with valid parameters' do
+      it "creates a new user" do
+        member = create(:member)
+        post :create, member: { name: member.name, email: member.email }, format: :json
+        expect(Member.last).to eq(member)
+      end
+    end
+    
+    context 'without valid parameters' do
+      it 'returns 302 status' do
+        post :create, member: { name: '', email: '' }, format: :json
+        expect(response).to have_http_status(422)
+      end
     end
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
+    xit "returns http success" do
       get :destroy
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #update" do
-    it "returns http success" do
+    xit "returns http success" do
       get :update
       expect(response).to have_http_status(:success)
     end
