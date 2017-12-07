5d9cf59a196569648764a4846893895e492071ee
Luis Henrique Witz <luis.witz@xing.com>
2017-12-06 23:26:26 -0200

    Refactor to send error mail

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -4,15 +4,14 @@ class CampaignRaffleJob < ApplicationJob
   def perform(campaign)
     results = RaffleService.new(campaign).call
 
-    campaign.members.each {|m| m.set_pixel}
-    results.each do |r|
-      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
-    end
-    campaign.update(status: :finished)
-
-
     if results == false
-      # Send mail to owner of campaign 
+      CampaignMailer.error(campaign, campaign.user).deliver_now
+    else
+      campaign.members.each {|m| m.set_pixel}
+      results.each do |r|
+        CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+      end
+      campaign.update(status: :finished)
     end
   end
 end
