cf8a687d5f74c5d0e27332c4c4678b9882253b6b
Luis Henrique Witz <luis.witz@xing.com>
2017-12-08 19:55:54 -0200

    Add some specs to job campaign

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  describe '#perform' do
+    let(:campaign) { create(:campaign) }
+
+    subject(:job) { described_class.perform_later(campaign) }
+
+    it 'queues the job' do
+      expect { job }.to have_enqueued_job
+    end
+  end
 end
